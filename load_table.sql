639f565cc3e82d17c32cd870e3a99505a166244f
makoo700 <96526496+makoo700@users.noreply.github.com>
2026-03-26 16:49:17 +0300

    Delete load_table.sql

--- load_table.sql
+++
@@ -1,7 +0,0 @@
-COPY allseasons
-FROM 'C:\Users\PC\Desktop\Developer\nba stats\all_seasons.csv' WITH (
-        FORMAT csv,
-        HEADER true,
-        DELIMITER ',',
-        ENCODING 'UTF8'
-    );
